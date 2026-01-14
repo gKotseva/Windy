6b9cfeeeadda3e9592eb1d74aad5a330d38474ad
Gabriela Kotseva <gkotseva98@gmail.com>
2026-01-14 15:31:19 +0200

    Needed dependencies installed and setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import "./index.css";
 
-import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
