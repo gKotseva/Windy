4148bd34e0fa34db568fc0528fe1102c19f28712
Gabriela Kotseva <gkotseva98@gmail.com>
2026-01-15 18:00:24 +0200

    Component update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,7 @@
 export const Search = () => {
   return (
     <div>
-      <input
-        placeholder="Search for cities"
-        // className="rounded-sm bg-[rgb(var(--input))] px-3 py-1"
-        className="input-style"
-      ></input>
+      <input placeholder="Search for cities" className="input-style"></input>
     </div>
   );
 };
