9b6c6825e2a5428b8c1a852d6f4b62a25b58b9d1
Gabriela Kotseva <gkotseva98@gmail.com>
2026-01-19 15:38:47 +0200

    Styling changes

--- src/components/TimeForecastContainer.jsx
+++ src/components/TimeForecastContainer.jsx
@@ -2,7 +2,7 @@ import { TimeForecastCard } from "./TimeForecastCard";
 
 export const TimeForecastContainer = () => {
   return (
-    <div className="field p-10">
+    <div className="field">
       <div className="uppercase mb-[30px] font-bold">Today's Forecast</div>
       <div className="flex divide-x divide-white/20">
         <TimeForecastCard />
