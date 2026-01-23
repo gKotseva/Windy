e1361781ed66afc57e7b0802a08896ead4908d0f
Gabriela Kotseva <gkotseva98@gmail.com>
2026-01-23 23:23:35 +0200

    New page created

---
+++ src/pages/Main.jsx
@@ -0,0 +1,25 @@
+import { AirConditions } from "../components/AirConditions";
+import { CurrentWeather } from "../components/CurrentWeather";
+import { Search } from "../components/Search";
+import { SevenDayForecastContainer } from "../components/SevenDayForecastContainer";
+import { Sidebar } from "../components/Sidebar";
+import { TimeForecastContainer } from "../components/TimeForecastContainer";
+
+export const Main = () => {
+  return (
+    <div className="flex m-5 w-full justify-between">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="flex flex-col gap-3">
+        <Search />
+        <CurrentWeather />
+        <TimeForecastContainer />
+        <AirConditions />
+      </div>
+      <div>
+        <SevenDayForecastContainer />
+      </div>
+    </div>
+  );
+};
