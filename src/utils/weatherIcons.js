2ea1469cd88ce4af802a95d16a44a9e220c8e084
Gabriela Kotseva <gkotseva98@gmail.com>
2026-01-27 16:06:54 +0200

    Helper function for icon view
    Added a helper function that will display an icon based on time and weather code received by API

---
+++ src/utils/weatherIcons.js
@@ -0,0 +1,53 @@
+import {
+  WiDaySunny,
+  WiDaySunnyOvercast,
+  WiDayCloudy,
+  WiCloudy,
+  WiRain,
+  WiRainWind,
+  WiSnow,
+  WiThunderstorm,
+  WiFog,
+  WiNightClear,
+  WiNightAltCloudy,
+  WiNightAltPartlyCloudy,
+} from "react-icons/wi";
+
+const dayIcons = {
+  0: WiDaySunny,
+  1: WiDaySunnyOvercast,
+  2: WiDayCloudy,
+  3: WiCloudy,
+  45: WiFog,
+  48: WiFog,
+  51: WiRain,
+  53: WiRain,
+  55: WiRain,
+  61: WiRain,
+  63: WiRainWind,
+  65: WiRainWind,
+  71: WiSnow,
+  73: WiSnow,
+  75: WiSnow,
+  95: WiThunderstorm,
+  96: WiThunderstorm,
+  99: WiThunderstorm,
+};
+
+const nightIcons = {
+  0: WiNightClear,
+  1: WiNightAltCloudy,
+  2: WiNightAltPartlyCloudy,
+  3: WiCloudy,
+};
+
+export const getWeatherIcon = (code, time) => {
+  const hour = parseInt(time.split(":")[0]);
+  const isNight = hour < 6 || hour >= 18;
+
+  if (isNight && nightIcons[code]) {
+    return nightIcons[code];
+  }
+
+  return dayIcons[code] || WiDaySunny;
+};
