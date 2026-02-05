39d12b2ce952b9ec6276d2cbc285c2bd8a22329e
Gabriela Kotseva <gkotseva98@gmail.com>
2026-02-05 15:44:59 +0200

    Updated components to use the response from the weather api

--- src/components/SevenDayForecastCard.jsx
+++ src/components/SevenDayForecastCard.jsx
@@ -1,14 +1,22 @@
-import { FaCloud } from "react-icons/fa";
+import { getWeatherIcon } from "../utils/weatherIcons";
+import { getWeatherType } from "../utils/weatherType";
 
-export const SevenDayForecastCard = () => {
+export const SevenDayForecastCard = ({ date, minTemp, maxTemp, code }) => {
+  const Icon = getWeatherIcon(code);
+
   return (
-    <div className="flex gap-10 items-center py-6 w-100 justify-center">
-      <h3 className="font-bold">Today</h3>
-      <div className="flex items-center gap-2">
-        <FaCloud color="white" size={50} />
-        <p className="text-white">Cloudy</p>
+    <div className="flex items-center py-6 w-90">
+      <h3 className="font-bold text-left w-32">
+        {new Date(date).toLocaleDateString("en-US", { weekday: "long" })}
+      </h3>
+      <div className="flex items-center justify-center w-48 gap-2">
+        <Icon size={50} className="text-white" />
+        <p className="text-white whitespace-nowrap">{getWeatherType(code)}</p>
       </div>
-      <h3 className="text-white">25&#176;/22&#176;</h3>
+      <h3 className="text-white text-right w-32 whitespace-nowrap">
+        {maxTemp.toFixed()}° / {""}
+        <span className="secondary-text">{minTemp.toFixed()}°</span>
+      </h3>
     </div>
   );
 };
