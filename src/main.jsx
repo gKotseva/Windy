8df096f28b6cf184bfc437d61c38d622301c55cb
Gabriela Kotseva <gkotseva98@gmail.com>
2026-02-05 15:44:40 +0200

    Wrapped main in the react query provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,19 @@ import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/Theme.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WeatherProvider } from "./context/Weather.jsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <WeatherProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </WeatherProvider>
+  </QueryClientProvider>
 );
